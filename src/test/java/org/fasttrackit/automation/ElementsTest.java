0b157a5e7647661fc3609d443c7b2406142ae3fa
Daniel Nutu <danielnutu23@gmail.com>
2017-02-09 21:11:24 +0200

    extract elements in ElementsView(checkbox&label)

--- src/test/java/org/fasttrackit/automation/ElementsTest.java
+++ src/test/java/org/fasttrackit/automation/ElementsTest.java
@@ -1,7 +1,4 @@
 package org.fasttrackit.automation;
-import com.sdl.selenium.web.SearchType;
-import com.sdl.selenium.web.WebLocator;
-import com.sdl.selenium.web.form.CheckBox;
 import org.fasttrackit.util.TestBase;
 import org.testng.annotations.Test;
 import static org.hamcrest.core.Is.is;
@@ -10,11 +7,7 @@ import static org.junit.Assert.assertThat;
 public class ElementsTest extends TestBase {
 
     private LoginView loginView = new LoginView();
-
-    private WebLocator stopProcessLabel = new WebLocator().setText("Stop the process?", SearchType.TRIM);
-    private WebLocator withEnterLabel = new WebLocator().setText("Label with Enter.", SearchType.TRIM, SearchType.CHILD_NODE);
-    private CheckBox stopProcessCheckbox = new CheckBox().setElPath("//form[1]/div[3]//input");
-    private CheckBox labelWithEnterCheckbox = new CheckBox().setElPath("//form[1]/div[4]/label/input");
+    private ElementsView page = new ElementsView();
 
     @Test
     public void checkboxesTest(){
@@ -22,18 +15,18 @@ public class ElementsTest extends TestBase {
         openLoginPage();
         loginView.login("eu@fast.com", "eu.pass");
 
-        stopProcessCheckbox.click();
-        labelWithEnterCheckbox.click();
+        page.stopProcessCheckbox.click();
+        page.labelWithEnterCheckbox.click();
 
         com.sdl.selenium.web.utils.Utils.sleep(2000);
-        stopProcessLabel.click();
-        withEnterLabel.click();
+        page.stopProcessLabel.click();
+        page.withEnterLabel.click();
 
         com.sdl.selenium.web.utils.Utils.sleep(2000);
-        stopProcessLabel.click();
-        withEnterLabel.click();
+        page.stopProcessLabel.click();
+        page.withEnterLabel.click();
 
-        assertThat("Stop the process is not selected!", stopProcessCheckbox.isSelected(), is(true));
-        assertThat("Label with Enter.", stopProcessCheckbox.isSelected(), is(true));
+        assertThat("Stop the process is not selected!", page.stopProcessCheckbox.isSelected(), is(true));
+        assertThat("Label with Enter.", page.stopProcessCheckbox.isSelected(), is(true));
     }
 }
