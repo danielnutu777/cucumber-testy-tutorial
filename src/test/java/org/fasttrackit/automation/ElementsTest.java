917686c6784d05f05e71fe3df3834b503d3389f2
Daniel Nutu <danielnutu23@gmail.com>
2017-02-09 20:38:45 +0200

    extract elements in class definiton

--- src/test/java/org/fasttrackit/automation/ElementsTest.java
+++ src/test/java/org/fasttrackit/automation/ElementsTest.java
@@ -4,8 +4,6 @@ import com.sdl.selenium.web.WebLocator;
 import com.sdl.selenium.web.form.CheckBox;
 import org.fasttrackit.util.TestBase;
 import org.testng.annotations.Test;
-import org.testng.internal.Utils;
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -13,17 +11,16 @@ public class ElementsTest extends TestBase {
 
     private LoginView loginView = new LoginView();
 
+    private WebLocator stopProcessLabel = new WebLocator().setText("Stop the process?", SearchType.TRIM);
+    private WebLocator withEnterLabel = new WebLocator().setText("Label with Enter.", SearchType.TRIM, SearchType.CHILD_NODE);
+    private CheckBox stopProcessCheckbox = new CheckBox().setElPath("//form[1]/div[3]//input");
+    private CheckBox labelWithEnterCheckbox = new CheckBox().setElPath("//form[1]/div[4]/label/input");
+
     @Test
     public void checkboxesTest(){
 
         openLoginPage();
         loginView.login("eu@fast.com", "eu.pass");
-
-        CheckBox stopProcessCheckbox = new CheckBox().setElPath("//form[1]/div[3]//input");
-        CheckBox labelWithEnterCheckbox = new CheckBox().setElPath("//form[1]/div[4]/label/input");
-
-        WebLocator stopProcessLabel = new WebLocator().setText("Stop the process?", SearchType.TRIM);
-        WebLocator withEnterLabel = new WebLocator().setText("Label with Enter.", SearchType.TRIM, SearchType.CHILD_NODE);
 
         stopProcessCheckbox.click();
         labelWithEnterCheckbox.click();
@@ -37,5 +34,6 @@ public class ElementsTest extends TestBase {
         withEnterLabel.click();
 
         assertThat("Stop the process is not selected!", stopProcessCheckbox.isSelected(), is(true));
+        assertThat("Label with Enter.", stopProcessCheckbox.isSelected(), is(true));
     }
 }
